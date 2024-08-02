fa06344cfede32e50afe8ee2d4c805af44e4b1c1
Eddie Blundell <eblundell@gmail.com>
2024-08-02 13:17:43 +0100

    Add missing header

---
+++ emulator.h
@@ -0,0 +1,50 @@
+#include <stdint.h>
+
+#pragma once
+
+#define MAX_DEVICE 4
+
+struct device {
+
+    uint16_t mem_range[2];
+    char name[12];
+
+    void (*init)();
+    void (*write)(uint16_t address, uint8_t data);
+    uint8_t (*read)(uint16_t address);
+};
+
+// Remember, addresses are 16bit and datapaths are 8bits
+// The operand is 16bits for 16bit addressing
+
+struct machine {
+
+    uint8_t memory[65536];
+    uint16_t pc;
+
+    // Registers
+    uint16_t accumulator;
+    uint8_t status;
+
+    uint16_t a, b, c, d, e, f, g;
+
+    /*
+     * Status Register
+     *
+     * Lowest
+     * 0 - Zero Flag
+     * 1 - Carry Flag
+     * 2 - Sign Flag
+     * 3 - Overflow flag
+     * 4 - Parity Fla
+     */
+
+    // Interrupt register
+    uint8_t ir;
+
+    struct device *devices[MAX_DEVICE];
+    int device_count;
+};
+
+int emu_register_device(struct machine *m,
+                        struct device *dev);
